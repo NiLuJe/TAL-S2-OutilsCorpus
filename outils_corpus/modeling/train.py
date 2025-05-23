5d6f7e63e585a522b517ec9462abb4e502f75a4c
NiLuJe <ninuje@gmail.com>
2025-05-23 23:17:28 +0200

    feat: plot feature effect on the classifier's decision
    Fun little insight!

--- outils_corpus/modeling/train.py
+++ outils_corpus/modeling/train.py
@@ -3,6 +3,8 @@
 
 from loguru import logger
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from sklearn.linear_model import RidgeClassifier
 from sklearn.metrics import ConfusionMatrixDisplay
 import typer
@@ -13,7 +15,56 @@ from outils_corpus.features import extract_features
 app = typer.Typer()
 
 
-def train_models():
+def plot_clf_feature_effects(clf, X_train, target_names, feature_names) -> None:
+	"""
+	Plot the impact of specific features on the classifier's decision,
+	c.f., https://scikit-learn.org/stable/auto_examples/text/plot_document_classification_20newsgroups.html#model-without-metadata-stripping
+	"""
+
+	# learned coefficients weighted by frequency of appearance
+	average_feature_effects = clf.coef_ * np.asarray(X_train.mean(axis=0)).ravel()
+
+	for i, label in enumerate(target_names):
+		top5 = np.argsort(average_feature_effects[i])[-5:][::-1]
+		if i == 0:
+			top = pd.DataFrame(feature_names[top5], columns=[label])
+			top_indices = top5
+		else:
+			top[label] = feature_names[top5]
+			top_indices = np.concatenate((top_indices, top5), axis=None)
+	top_indices = np.unique(top_indices)
+	predictive_words = feature_names[top_indices]
+
+	# plot feature effects
+	bar_size = 0.25
+	padding = 0.75
+	y_locs = np.arange(len(top_indices)) * (4 * bar_size + padding)
+
+	fig, ax = plt.subplots(figsize=(10, 8))
+	for i, label in enumerate(target_names):
+		ax.barh(
+			y_locs + (i - 2) * bar_size,
+			average_feature_effects[i, top_indices],
+			height=bar_size,
+			label=label,
+		)
+	ax.set(
+		yticks=y_locs,
+		yticklabels=predictive_words,
+		ylim=[
+			0 - 4 * bar_size,
+			len(top_indices) * (4 * bar_size + padding) - 4 * bar_size,
+		],
+	)
+	ax.legend(loc="lower right")
+
+	print("top 5 keywords per class:")
+	print(top)
+
+	return ax
+
+
+def train_models() -> None:
 	"""
 	Train a bunch of models
 	"""
@@ -33,8 +84,11 @@ def train_models():
 	ConfusionMatrixDisplay.from_predictions(y_test, pred, ax=ax)
 	ax.xaxis.set_ticklabels(target_names)
 	ax.yaxis.set_ticklabels(target_names)
-	_ = ax.set_title(f"Confusion Matrix for {clf.__class__.__name__}\non the original documents")
+	_ = ax.set_title(f"Confusion Matrix for {clf.__class__.__name__}")
 	plt.savefig(FIGURES_DIR / "CLF-confusion-matrix.png")
+
+	_ = plot_clf_feature_effects(clf, X_train, target_names, feature_names).set_title("Average feature effect")
+	plt.savefig(FIGURES_DIR / "CLF-feature-effect.png", dpi=450)
 
 
 @app.command()
